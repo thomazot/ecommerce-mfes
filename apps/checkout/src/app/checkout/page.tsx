7faf7ed2f625f557da84587bf2a201336e41e601
Thomaz Toyama <contato@thomazot.com.br>
2025-07-14 15:12:14 -0300

    feat: implement page checkout

--- apps/checkout/src/app/checkout/page.tsx
+++ apps/checkout/src/app/checkout/page.tsx
@@ -3,7 +3,7 @@ import { CheckoutPage } from '@ecommerce-mfe/shared/containers/checkoutPage';
 export default function Page() {
   return (
     <>
-      home
+      checkout
       <CheckoutPage />
     </>
   );
