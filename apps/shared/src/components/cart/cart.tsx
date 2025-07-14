b45a0d2eece3912e70dde7d958ff29e082de7141
Thomaz Toyama <contato@thomazot.com.br>
2025-07-14 12:43:11 -0300

    feat: implement routes checkout

--- apps/shared/src/components/cart/cart.tsx
+++ apps/shared/src/components/cart/cart.tsx
@@ -6,7 +6,12 @@ import { Link } from '../../commons/link';
 const mockCount = 2;
 
 export const Cart: React.FC = () => (
-  <Link href="/cart" className={cart()} aria-label="Shopping cart" tabIndex={0}>
+  <Link
+    href="/checkout"
+    className={cart()}
+    aria-label="Shopping cart"
+    tabIndex={0}
+  >
     <ShoppingCartIcon className={cartIcon()} />
     <span className={cartBadge()} aria-label={`${mockCount} items in cart`}>
       {mockCount}
